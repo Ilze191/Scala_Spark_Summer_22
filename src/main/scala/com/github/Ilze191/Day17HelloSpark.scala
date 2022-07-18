07b8676aeb44b97cc30bf3c6bb702eea32de4bd6
Ilze <puisaane,ilze@gmail.com>
2022-07-18 17:53:06 +0300

    First steps with Spark

--- src/main/scala/com/github/Ilze191/Day17HelloSpark.scala
+++ src/main/scala/com/github/Ilze191/Day17HelloSpark.scala
@@ -6,15 +6,18 @@ object Day17HelloSpark extends App {
   println(s"Testing Scala version: ${util.Properties.versionString}")
 
   val spark = SparkSession.builder().appName("test").master("local").getOrCreate()
-  //also session is a common name for the above spark object
   println(s"Session started on Spark version ${spark.version}")
 
-  val myRange = spark.range(1000).toDF("number") //create a single column dataframe (table)
-  val divisibleBy5 = myRange.where("number % 5 = 0") //so similaraities with SQL and regular Scala
-  divisibleBy5.show(10) //show first 10 rows
+//  val myRange = spark.range(1000).toDF("number") //create a single column dataframe (table)
+//  val divisibleBy5 = myRange.where("number % 5 = 0") //so similaraities with SQL and regular Scala
+//  divisibleBy5.show(10) //show first 10 rows
 
   //TODO create range of numbers 0 to 100
+  val range = spark.range(100).toDF("number")
   //TODO filter into numbers divisible by 10
+  val divisibleBy10 = range.where("number % 10 = 0")
   //TODO show the results
+  divisibleBy10.show
+  spark.stop()
 
 }
