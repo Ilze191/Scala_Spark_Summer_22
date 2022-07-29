331dc7446e14b64963448f45b6bddf353de3a99f
Ilze <puisaane,ilze@gmail.com>
2022-07-29 23:15:36 +0300

    Day 23 exercise 1

--- src/main/scala/com/github/Ilze191/Day23Exercise.scala
+++ src/main/scala/com/github/Ilze191/Day23Exercise.scala
@@ -16,9 +16,10 @@ object Day23Exercise extends App {
   //TODO get all purchases that have been made from Finland
   //TODO sort by Unit Price and LIMIT 20
   //TODO collect results into an Array of Rows
-  //print these 20 Rows
+
  val finlandRows20 =  spark.sql("SELECT * FROM marchTable WHERE Country = 'Finland' ORDER BY UnitPrice LIMIT 20").collect()
 
+  //print these 20 Rows
   for ((row, i) <- finlandRows20.zipWithIndex) {
  println(s"Row No. ${i+1} -> $row")
      }
