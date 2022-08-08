51cf406c520b361f86839a580b43bd673103b9da
Ilze <puisaane,ilze@gmail.com>
2022-08-08 17:33:35 +0300

    Day 27 Exercise

--- src/main/scala/com/github/Ilze191/Day27Exercise.scala
+++ src/main/scala/com/github/Ilze191/Day27Exercise.scala
@@ -1,6 +1,5 @@
 package com.github.Ilze191
-import com.github.Ilze191.SparkUtil.getSpark
-
+import com.github.Ilze191.SparkUtil.{getSpark, readDataWithView}
 import org.apache.spark.sql.functions.{col, udf}
 object Day27Exercise extends App {
 
@@ -23,9 +22,46 @@ object Day27Exercise extends App {
    // .show(21)
     .show(tempDF.count.toInt)
 
+
   //TODO simple task find count, distinct count and also aproximate distinct count (with default RSD)
   // for InvoiceNo, CustomerID AND UnitPrice columns
   //of course count should be the same for all of these because that is the number of rows
+
+  val filePath = "src/resources/retail-data/all/online-retail-dataset.csv" //here it is a single file but wildcard should still work
+  val df = readDataWithView(spark, filePath)
+
+
+  spark.sql(
+    """
+      |SELECT count(InvoiceNo),
+      |count(CustomerID),
+      |count(UnitPrice)
+      |FROM dfTable
+      |""".stripMargin)
+    .show()
+
+
+  spark.sql(
+    """
+      |SELECT count(distinct(InvoiceNo)),
+      |count(distinct(CustomerID)),
+      |count(distinct(UnitPrice))
+      |FROM dfTable
+      |""".stripMargin)
+    .show()
+
+
+  spark.sql(
+    """
+      |SELECT approx_count_distinct(InvoiceNo),
+      |approx_count_distinct(CustomerID),
+      |approx_count_distinct(UnitPrice)
+      |FROM dfTable
+      |""".stripMargin)
+    .show()
+
+
+
 
 
 
